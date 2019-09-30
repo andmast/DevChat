52f0f91ad8af696ea4835112be91c23b9900b9cf
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-09-30 00:28:20 -0400

    Fixed Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,16 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
-var config = {
-  apiKey: "AIzaSyCREQMYIbAYKscrS_JYvWP4KpMsF_jI5Bg",
-  authDomain: "react-slack-clone.firebaseapp.com",
-  databaseURL: "https://react-slack-clone.firebaseio.com",
-  projectId: "react-slack-clone",
-  storageBucket: "react-slack-clone.appspot.com",
-  messagingSenderId: "423395756405"
+var firebaseConfig = {
+  apiKey: "AIzaSyBMOWfEtma2O1uCMhtr6ozADr-h5-9H-2I",
+  authDomain: "react-slack-clone-40d0a.firebaseapp.com",
+  databaseURL: "https://react-slack-clone-40d0a.firebaseio.com",
+  projectId: "react-slack-clone-40d0a",
+  storageBucket: "react-slack-clone-40d0a.appspot.com",
+  messagingSenderId: "663203630718",
+  appId: "1:663203630718:web:daefd2c620e8d0ce93d22f",
+  measurementId: "G-9KJJ5WJ2TC"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
