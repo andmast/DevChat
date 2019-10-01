ee29849ca411bfccd798c18462e33ee3bc013ac9
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-10-01 00:19:54 -0400

    meta panel scoffolded

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,41 @@
 import React from "react";
+import { Segment, Accordion, Header, Icon } from "semantic-ui-react";
 
 class MetaPanel extends React.Component {
+  state = {
+    activeIndex: 0
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>MetaPanel</div>;
+    const { activeIndex } = this.state;
+    return (
+      <Segment>
+        <Header as="h3" attached="top">
+          About # Channel
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="info" />
+            Channels Details
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            Details
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
